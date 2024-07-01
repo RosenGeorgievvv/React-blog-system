ccbb811c4931a2ebc37b5263d1a302c47ad5850e
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-01 12:58:53 +0300

    Sidebar navigation completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <span className="header">F2F Chat</span>
+      <div className="user">
+        <img src="https://media.licdn.com/dms/image/D4D03AQFt0Phvs79Lvg/profile-displayphoto-shrink_200_200/0/1712508041785?e=2147483647&v=beta&t=TV9CggKT-83KBKn0QLsRisVdJTZreKgmrecOsuHjabQ" alt="" />
+        <span>Rosen</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
