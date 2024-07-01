754156436fd4be840daf9b30c656fdb8a9798f1f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-01 12:11:12 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import addAvatar from '../assets/addAvatar.png';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='main-form'>
+        <div className='main-wrapper'>
+            <span className='header'>F2F Chat</span>
+            <span className='title'>Login</span>
+            <form>
+                <input type='email' placeholder='email'/>
+                <input type='password' placeholder='password'/>
+                <button>Sign in</button>
+            </form>
+            <p>You do not have an account? Register</p>
+        </div>
+    </div>
   )
 }
 
