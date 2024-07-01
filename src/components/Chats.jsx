714529552b2d188ceb7340956f6a5cbc5b354270
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-01 17:27:16 +0300

    display chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="user-chat">
+        <img
+          src="https://media.licdn.com/dms/image/D4D03AQFt0Phvs79Lvg/profile-displayphoto-shrink_200_200/0/1712508041785?e=2147483647&v=beta&t=TV9CggKT-83KBKn0QLsRisVdJTZreKgmrecOsuHjabQ"
+          alt=""
+        />
+        <div className="chat-info">
+          <span>Pesho</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
