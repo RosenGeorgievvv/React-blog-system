e8a51e2ddfb80dc3033affd26477f764926636b5
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-01 13:08:59 +0300

    Search component styles improvement

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className='search-form'>
+        <input type='text' placeholder='find a user' />
+      </div>
+      <div className="user-chat">
+        <img src="https://media.licdn.com/dms/image/D4D03AQFt0Phvs79Lvg/profile-displayphoto-shrink_200_200/0/1712508041785?e=2147483647&v=beta&t=TV9CggKT-83KBKn0QLsRisVdJTZreKgmrecOsuHjabQ" alt="" />
+        <div className='chat-info'>
+          <span>Pesho</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
