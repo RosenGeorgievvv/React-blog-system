f625f722f0302c7176a406d2c2b5fe908e2cc7e8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-01 12:14:15 +0300

    Home page implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './styles.scss';
 
 import Register from "./pages/Register";
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
 
   return (
     <>
-    <Register />
+    <Home />
     </>
   )
 }
