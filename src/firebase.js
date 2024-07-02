ab2152858bd7a398d8b54a55058ef229fa2a9982
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-02 12:25:20 +0300

    Update firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAE38InVm6Y4q0SygQiCibKMvyCuf1XG0",
+  authDomain: "chat-app-c9852.firebaseapp.com",
+  projectId: "chat-app-c9852",
+  storageBucket: "chat-app-c9852.appspot.com",
+  messagingSenderId: "768750861257",
+  appId: "1:768750861257:web:f3ebd623fcf219042ac618"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
