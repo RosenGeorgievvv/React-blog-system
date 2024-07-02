05df9519163a4d91915ccdacadd94ee6ca86a1d1
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-02 12:17:55 +0300

    project update

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className='messageInfo'>
         <img src="https://media.licdn.com/dms/image/D4D03AQFt0Phvs79Lvg/profile-displayphoto-shrink_200_200/0/1712508041785?e=2147483647&v=beta&t=TV9CggKT-83KBKn0QLsRisVdJTZreKgmrecOsuHjabQ" alt="" />
       <span>just now</span>
