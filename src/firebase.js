845802049689f2de635ac45ddc8af8632fd41b08
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-02 12:38:44 +0300

    Firebase authentication enabled

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCAE38InVm6Y4q0SygQiCibKMvyCuf1XG0",
@@ -7,8 +7,9 @@ const firebaseConfig = {
   projectId: "chat-app-c9852",
   storageBucket: "chat-app-c9852.appspot.com",
   messagingSenderId: "768750861257",
-  appId: "1:768750861257:web:f3ebd623fcf219042ac618"
+  appId: "1:768750861257:web:f3ebd623fcf219042ac618",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
-import addAvatar from '../assets/addAvatar.png';
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import addAvatar from "../assets/addAvatar.png";
 
 const Register = () => {
-  return (
-    <div className='main-form'>
-        <div className='main-wrapper'>
-            <span className='header'>F2F Chat</span>
-            <span className='title'>Register</span>
-            <form>
-                <input type='text' placeholder='name'/>
-                <input type='email' placeholder='email'/>
-                <input type='password' placeholder='password'/>
-                <input style={{display: 'none'}} type='file' id='file' />
-                 <label htmlFor='file'>
-                    <img src={addAvatar} alt="avatar" />
-                 <span>Add an Avatar</span>
-                 </label>
-                <button>Sign up</button>
-            </form>
-            <p>You do have an account? Login</p>
-        </div>
-    </div>
-  )
-}
+  const [err, setErr] = useState(false);
 
-export default Register
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[1];
+
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  return (
+    <div className="main-form">
+      <div className="main-wrapper">
+        <span className="header">F2F Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={addAvatar} alt="avatar" />
+            <span>Add an Avatar</span>
+          </label>
+          <button>Sign up</button>
+          {err && <span>Something went wrong...</span>}
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
