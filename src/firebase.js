f850d57232e313ac92543c1eb1dceac553d1d840
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-07-04 16:15:32 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCAE38InVm6Y4q0SygQiCibKMvyCuf1XG0",
